454789e03f23803802d38dd52092a03891c559e6
Alan <tyujn24@mail.ru>
2024-01-20 00:22:00 +0300

    shit

--- src/view/pages/Landing/index.tsx
+++ src/view/pages/Landing/index.tsx
@@ -4,6 +4,9 @@ import styles from './index.module.scss';
 import Icon from '../../components/UI/Icon';
 import { ICON } from '../../components/UI/Icon/IconType.tsx';
 import { useGetProductsQuery } from '../../../core/api/product';
+import { FactoryImage } from '../../../assets/image/FactoryImage';
+import { ContentWrapper } from '../../components/ContentWrapper';
+import { Header } from '../../components/Header';
 
 const cx = classNames.bind(styles);
 
@@ -13,8 +16,10 @@ export const Landing: React.FC = () => {
     console.log(data);
     return (
         <div className={cx(styles.landing)}>
-            <div className={'container block'}></div>
-            <Icon icon={ICON.LOGO} width={175} height={55} color={'black'} />
+            <ContentWrapper>
+                <Header />
+                <FactoryImage />
+            </ContentWrapper>
         </div>
     );
 };

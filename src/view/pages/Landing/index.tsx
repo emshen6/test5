e7b13410b19a2dda0b9b1b5a50266f353450fdb6
nekruz03 <nekruz.nazirjonov@mail.ru>
2024-01-28 14:34:47 +0300

    fix

--- src/view/pages/Landing/index.tsx
+++ src/view/pages/Landing/index.tsx
@@ -3,21 +3,14 @@ import classNames from 'classnames';
 import styles from './index.module.scss';
 import { FactoryImage } from '../../../assets/image/FactoryImage';
 import { Header } from '../../components/Header';
-import { Footer } from '../../components/Footer/index.tsx';
-import { Benefits } from '../../components/Benefits/index.tsx';
+import { Benefits } from '../../components/Benefits';
 import { Footer } from '../../components/Footer';
 import { About } from '../../components/About';
 import { CatalogPart } from '../../components/CatalogPart';
-import { useGetProductsQuery } from '../../../core/api/product';
 
 const cx = classNames.bind(styles);
 
 export const Landing: React.FC = () => {
-    const titles = ['a', 'b', 'c', 'd'];
-    const descriptions = ['a', 'b', 'c', 'd'];
-
-    console.log(data);
-
     return (
         <div className={cx(styles.landing)}>
             <div className={styles.wrapper}>
